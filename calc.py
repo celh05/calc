725fe8942e6751d8e956cafe2d62e7c4c7892c5d
celh05 <cel.harvey@gmail.com>
2017-09-30 10:55:15 -0500

    My first commit

---
+++ calc.py
@@ -0,0 +1,9 @@
+print"Math Calculator"
+print"Enter two numbers you will like to add"
+
+print "Enter first number"
+firstNum = input()
+print "Enter second number"
+secondNum = input()
+
+print "The total is ", firstNum + secondNum
